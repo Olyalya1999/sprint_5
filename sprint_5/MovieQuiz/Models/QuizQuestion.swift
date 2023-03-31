5e624bb8b4768403bb9aff433e3013f020e29993
Olya <109566838+Olyalya1999@users.noreply.github.com>
2023-03-31 01:17:59 -0400

    Add files via upload

---
+++ sprint_5/MovieQuiz/Models/QuizQuestion.swift
@@ -0,0 +1,14 @@
+//
+//  QuizQuestion.swift
+//  MovieQuiz
+//
+//  Created by Olya on 27.03.2023.
+//
+
+import Foundation
+
+struct QuizQuestion {
+    let image: String
+    let text: String
+    let correctAnswer: Bool
+}
---
+++ sprint_5/MovieQuiz/Services/QuestionFactoryProtocol.swift
@@ -0,0 +1,13 @@
+//
+//  QuestionfactoryProtocol.swift
+//  MovieQuiz
+//
+//  Created by Olya on 27.03.2023.
+//
+
+import Foundation
+
+
+protocol  QuestionFactoryProtocol {
+    func requestNextQuestion()
+}
